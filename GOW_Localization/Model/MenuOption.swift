5ca7bd16182188e4422bd31c0d1cc7343d444225
Azul Alfaro <azul.alfaro.fernandez@gmail.com>
2024-08-31 08:21:38 -0600

    AAA

---
+++ GOW_Localization/Model/MenuOption.swift
@@ -0,0 +1,8 @@
+//
+//  MenuOption.swift
+//  GOW_Localization
+//
+//  Created by Azul Alfaro on 30/08/24.
+//
+
+import Foundation
---
+++ GOW_Localization/View/GOWCell.swift
@@ -0,0 +1,23 @@
+//
+//  GOWCell.swift
+//  GOW_Localization
+//
+//  Created by Azul Alfaro on 30/08/24.
+//
+
+import UIKit
+
+class GOWCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
